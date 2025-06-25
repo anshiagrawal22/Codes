7d66db96e8f05887103e06765b912f85d580d1d9
Anshi Agrawal <147601739+anshiagrawal22@users.noreply.github.com>
2025-06-25 23:51:29 +0530

    Added Codes

---
+++ java/Strings/Basic.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class Basic {
+    public static void main(String[] args) {
+        String name=new String("Name"); //object
+        String surname="Surname"; //can be used as data type
+        System.out.println(name+" "+surname);
+        int a=3; 
+        System.out.printf("This is a format Specifiers and value of a: %d",a);
+    }
+}
